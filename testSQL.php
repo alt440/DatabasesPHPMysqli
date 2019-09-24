2886f100cf7d0f22d4e7042485abce0438b72f6a
Daniel Vigny-Pau <35085737+danielvignypau@users.noreply.github.com>
2019-09-24 10:48:53 -0400

    drop tables at beginning, print all tables, print examples of accessing data from tables

--- testSQL.php
+++ testSQL.php
@@ -86,8 +86,8 @@
 	$empty = "No results";
 
 	echo '<h2>'.$title1.'</h2>';
-	
-	$peopleHeader = "People";
+
+	$peopleHeader = "TABLE 1: People";
 	echo '<h3>'.$peopleHeader.'</h3>';
 	//printing the table of people, ordered by ID in ascending order
 	echo '<table border="1" width="50%">';
@@ -109,7 +109,7 @@
 	echo '</table>';
 	echo '</br>';
 
-	$eventHeader = "Event";
+	$eventHeader = "TABLE 2: Event";
 	echo '<h3>'.$eventHeader.'</h3>';
 	//printing the table of people, ordered by ID in ascending order
 	echo '<table border="1" width="50%">';
@@ -131,7 +131,7 @@
 	echo '</table>';
 	echo '</br>';
 
-	$role_of_people_in_the_eventHeader = "Role of people in the event";
+	$role_of_people_in_the_eventHeader = "TABLE 3: Role of people in the event";
 	echo '<h3>'.$role_of_people_in_the_eventHeader.'</h3>';
 	//printing the table of role of people in the event, ordered by ID in ascending order
 	echo '<table border="1" width="50%">';
@@ -139,6 +139,190 @@
 	echo '<tr><th>ID</th><th>User ID</th><th>Event ID</th></tr>';
 	//selecting data from the table
 	$queryRole = "SELECT * FROM role_of_people_in_the_event ORDER BY id ASC";
+	$resultRole = $mysqli->query($queryRole);
+	//checking if the results from the query return any rows
+	if ($resultRole->num_rows > 0) {
+		while ($rowRole= $resultRole->fetch_assoc()) {
+			//printing the table row with the data of each entry in the table 'Role'
+			echo '<tr><td>'.$rowRole['id'].'</td><td>'.$rowRole['userid'].'</td><td>'.$rowRole['EventID'].'</td></tr>';}
+	} else {
+		//print that there were no results if there are 0 resulting rows
+		echo '<tr><td>'.$empty.'</td></tr>';
+	}
+	echo '</table>';
+	echo '</br>';
+
+	//EXAMPLES OF ACCESSING DATA IN THE TABLES
+
+	//table with people, only displaying first name, last name, and middle name
+	$peopleHeader = "TABLE 4: People - only displaying first name, last name, and middle name, sorted by last name reverse alphabetically";
+	echo '<h3>'.$peopleHeader.'</h3>';
+	//printing the table of people, ordered by ID in ascending order
+	echo '<table border="1" width="50%">';
+	//printing the table header
+	echo '<tr><th>Last name</th><th>First name</th><th>Middle name</th></tr>';
+	//selecting data from the table
+	$queryPeople = "SELECT firstname, lastname, middle_name FROM people ORDER BY lastname DESC";
+	$resultPeople = $mysqli->query($queryPeople);
+	//checking if the results from the query return any rows
+	if ($resultPeople->num_rows > 0) {
+		while ($rowPeople = $resultPeople->fetch_assoc()) {
+			//printing the table row with the data of each entry in the table 'people'
+			echo '<tr><td>'.$rowPeople['lastname'].'</td><td>'.$rowPeople['firstname'].'</td><td>'.$rowPeople['middle_name'].'</td></tr>';
+		}
+	} else {
+		//print that there were no results if there are 0 resulting rows
+		echo '<tr><td>'.$empty.'</td></tr>';
+	}
+	echo '</table>';
+	echo '</br>';
+
+	//table with people with a middle name only
+	$peopleHeader = "TABLE 5: People - only people with middle names, ordered by middle name alphabetically";
+	echo '<h3>'.$peopleHeader.'</h3>';
+	//printing the table of people, ordered by ID in ascending order
+	echo '<table border="1" width="50%">';
+	//printing the table header
+	echo '<tr><th>ID</th><th>Last name</th><th>First name</th><th>Middle name</th><th>User ID</th><th>Password</th></tr>';
+	//selecting data from the table
+	$queryPeople = "SELECT * FROM people WHERE middle_name != '' ORDER BY middle_name ASC";
+	$resultPeople = $mysqli->query($queryPeople);
+	//checking if the results from the query return any rows
+	if ($resultPeople->num_rows > 0) {
+		while ($rowPeople = $resultPeople->fetch_assoc()) {
+			//printing the table row with the data of each entry in the table 'people'
+			echo '<tr><td>'.$rowPeople['id'].'</td><td>'.$rowPeople['lastname'].'</td><td>'.$rowPeople['firstname'].'</td><td>'.$rowPeople['middle_name'].'</td><td>'.$rowPeople['userID'].'</td><td>'.$rowPeople['password'].'</td></tr>';
+		}
+	} else {
+		//print that there were no results if there are 0 resulting rows
+		echo '<tr><td>'.$empty.'</td></tr>';
+	}
+	echo '</table>';
+	echo '</br>';
+
+	//table with people with a last name that starts with the letter 'L'
+	$peopleHeader = "TABLE 6: People - only people where last name starts with 'L', ordered by last name alphabetically";
+	echo '<h3>'.$peopleHeader.'</h3>';
+	//printing the table of people, ordered by ID in ascending order
+	echo '<table border="1" width="50%">';
+	//printing the table header
+	echo '<tr><th>ID</th><th>Last name</th><th>First name</th><th>Middle name</th><th>User ID</th><th>Password</th></tr>';
+	//selecting data from the table
+	$queryPeople = "SELECT * FROM people WHERE lastname LIKE 'L%' ORDER BY lastname ASC";
+	$resultPeople = $mysqli->query($queryPeople);
+	//checking if the results from the query return any rows
+	if ($resultPeople->num_rows > 0) {
+		while ($rowPeople = $resultPeople->fetch_assoc()) {
+			//printing the table row with the data of each entry in the table 'people'
+			echo '<tr><td>'.$rowPeople['id'].'</td><td>'.$rowPeople['lastname'].'</td><td>'.$rowPeople['firstname'].'</td><td>'.$rowPeople['middle_name'].'</td><td>'.$rowPeople['userID'].'</td><td>'.$rowPeople['password'].'</td></tr>';
+		}
+	} else {
+		//print that there were no results if there are 0 resulting rows
+		echo '<tr><td>'.$empty.'</td></tr>';
+	}
+	echo '</table>';
+	echo '</br>';
+
+	//table with people with a user ID between 1000000 and 2000000
+	$peopleHeader = "TABLE 7: People - only people with User ID between 1000000 and 2000000, sorted by User ID";
+	echo '<h3>'.$peopleHeader.'</h3>';
+	//printing the table of people, ordered by ID in ascending order
+	echo '<table border="1" width="50%">';
+	//printing the table header
+	echo '<tr><th>ID</th><th>Last name</th><th>First name</th><th>Middle name</th><th>User ID</th><th>Password</th></tr>';
+	//selecting data from the table
+	$queryPeople = "SELECT * FROM people WHERE userID BETWEEN 1000000 AND 2000000 ORDER BY userID ASC";
+	$resultPeople = $mysqli->query($queryPeople);
+	//checking if the results from the query return any rows
+	if ($resultPeople->num_rows > 0) {
+		while ($rowPeople = $resultPeople->fetch_assoc()) {
+			//printing the table row with the data of each entry in the table 'people'
+			echo '<tr><td>'.$rowPeople['id'].'</td><td>'.$rowPeople['lastname'].'</td><td>'.$rowPeople['firstname'].'</td><td>'.$rowPeople['middle_name'].'</td><td>'.$rowPeople['userID'].'</td><td>'.$rowPeople['password'].'</td></tr>';
+		}
+	} else {
+		//print that there were no results if there are 0 resulting rows
+		echo '<tr><td>'.$empty.'</td></tr>';
+	}
+	echo '</table>';
+	echo '</br>';
+
+	//table with events with no end date
+	$eventHeader = "TABLE 8: Events with no end date";
+	echo '<h3>'.$eventHeader.'</h3>';
+	//printing the table of people, ordered by ID in ascending order
+	echo '<table border="1" width="50%">';
+	//printing the table header
+	echo '<tr><th>ID</th><th>Event</th><th>Event ID</th><th>Start date</th><th>End date</th><th>Admin user ID</th></tr>';
+	//selecting data from the table
+	$queryEvent = "SELECT * FROM event WHERE end_date = '' ORDER BY id ASC";
+	$resultEvent = $mysqli->query($queryEvent);
+	//checking if the results from the query return any rows
+	if ($resultEvent->num_rows > 0) {
+		while ($rowEvent = $resultEvent->fetch_assoc()) {
+			//printing the table row with the data of each entry in the table 'people'
+			echo '<tr><td>'.$rowEvent['id'].'</td><td>'.$rowEvent['Event'].'</td><td>'.$rowEvent['EventID'].'</td><td>'.$rowEvent['start_date'].'</td><td>'.$rowEvent['end_date'].'</td><td>'.$rowEvent['AdminUserID'].'</td></tr>';
+		}
+	} else {
+		//print that there were no results if there are 0 resulting rows
+		echo '<tr><td>'.$empty.'</td></tr>';
+	}
+	echo '</table>';
+	echo '</br>';
+
+	//table with events starting in the year 2011
+	$eventHeader = "TABLE 9: Events starting in the year 2011";
+	echo '<h3>'.$eventHeader.'</h3>';
+	//printing the table of people, ordered by ID in ascending order
+	echo '<table border="1" width="50%">';
+	//printing the table header
+	echo '<tr><th>ID</th><th>Event</th><th>Event ID</th><th>Start date</th><th>End date</th><th>Admin user ID</th></tr>';
+	//selecting data from the table
+	$queryEvent = "SELECT * FROM event WHERE start_date LIKE '2011%' ORDER BY id ASC";
+	$resultEvent = $mysqli->query($queryEvent);
+	//checking if the results from the query return any rows
+	if ($resultEvent->num_rows > 0) {
+		while ($rowEvent = $resultEvent->fetch_assoc()) {
+			//printing the table row with the data of each entry in the table 'people'
+			echo '<tr><td>'.$rowEvent['id'].'</td><td>'.$rowEvent['Event'].'</td><td>'.$rowEvent['EventID'].'</td><td>'.$rowEvent['start_date'].'</td><td>'.$rowEvent['end_date'].'</td><td>'.$rowEvent['AdminUserID'].'</td></tr>';
+		}
+	} else {
+		//print that there were no results if there are 0 resulting rows
+		echo '<tr><td>'.$empty.'</td></tr>';
+	}
+	echo '</table>';
+	echo '</br>';
+
+	//table with events with users 3060862 and 7034113
+	$role_of_people_in_the_eventHeader = "TABLE 10: Role of people in the event only with users 3060862 and 7034113";
+	echo '<h3>'.$role_of_people_in_the_eventHeader.'</h3>';
+	//printing the table of role of people in the event, ordered by ID in ascending order
+	echo '<table border="1" width="50%">';
+	//printing the table header
+	echo '<tr><th>ID</th><th>User ID</th><th>Event ID</th></tr>';
+	//selecting data from the table
+	$queryRole = "SELECT * FROM role_of_people_in_the_event WHERE userid = 3060862 OR userid = 7034113 ORDER BY id ASC";
+	$resultRole = $mysqli->query($queryRole);
+	//checking if the results from the query return any rows
+	if ($resultRole->num_rows > 0) {
+		while ($rowRole= $resultRole->fetch_assoc()) {
+			//printing the table row with the data of each entry in the table 'Role'
+			echo '<tr><td>'.$rowRole['id'].'</td><td>'.$rowRole['userid'].'</td><td>'.$rowRole['EventID'].'</td></tr>';}
+	} else {
+		//print that there were no results if there are 0 resulting rows
+		echo '<tr><td>'.$empty.'</td></tr>';
+	}
+	echo '</table>';
+	echo '</br>';
+
+	//table with events only with events with Event ID 4
+	$role_of_people_in_the_eventHeader = "TABLE 11: Role of people in the event only with Event ID 4";
+	echo '<h3>'.$role_of_people_in_the_eventHeader.'</h3>';
+	//printing the table of role of people in the event, ordered by ID in ascending order
+	echo '<table border="1" width="50%">';
+	//printing the table header
+	echo '<tr><th>ID</th><th>User ID</th><th>Event ID</th></tr>';
+	//selecting data from the table
+	$queryRole = "SELECT * FROM role_of_people_in_the_event WHERE EventID = 4 ORDER BY id ASC";
 	$resultRole = $mysqli->query($queryRole);
 	//checking if the results from the query return any rows
 	if ($resultRole->num_rows > 0) {
